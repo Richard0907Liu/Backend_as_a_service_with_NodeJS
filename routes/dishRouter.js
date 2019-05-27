3903291c8e8a0f9afadf7831281489b44c5517a8
Richard Liu <richard0907@gmail.com>
2019-05-27 13:57:26 -0400

    Express REST API with MongoDB and Mongoose Part 2

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -40,7 +40,7 @@ dishRouter.route('/')
     .catch((err) => next(err));
 })
 .put((req, res, next) => {
-    res.statusCode = 403; // 403 means their operation not supported
+    res.statusCode = 403; // 403 means their  operation not supported
     res.end('PUT operation not supported on /dishes');
 })
 .delete((req, res, next) => { // Later on, we will learn how to use authentication
@@ -86,6 +86,166 @@ dishRouter.route('/:dishId')
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+////////////
+// For /comments and /commentsId
+dishRouter.route('/:dishId/comments')  
+.get((req, res, next) => {
+    Dishes.findById(req.params.dishId)  // send to the mongodb server using a mongoose methed
+    .then((dish) => {
+        if(dish != null){
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(dish.comments);  // return the comments
+        }
+        else{ // dish not exist
+            err = new Error('Dish' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err); // we're just going to return that error here from the get operation
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        if(dish != null){
+            // Before pass back the value
+            dish.comments.push(req.body);
+            dish.save() // save the collection 
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);  // returning the updated dish back to the user here.
+            }, (err) => next(err));
+        }
+        else{ // dish not exist
+            err = new Error('Dish' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err); // we're just going to return that error here from the get operation
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.put((req, res, next) => {
+    res.statusCode = 403; // 403 means their  operation not supported
+    res.end('PUT operation not supported on /dishes'
+        + req.params.dishId + '/comments');
+})
+.delete((req, res, next) => { // Later on, we will learn how to use authentication
+    Dishes.findById(req.params.dishId)
+    .then((dish) => { // resp, Server response
+        if(dish != null){
+            /** have to go in and delete each of the comments */
+            for(var i = (dish.comments.length -1); i >=0; i--){ 
+                /** inside here (dish.comments[i]._id) you will specify the id of the subdocuments 
+                that you're trying to access*/
+                dish.comments.id(dish.comments[i]._id).remove();
+            }
+            dish.save() // save the collection 
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);  // returning the updated dish back to the user here.
+            }, (err) => next(err));
+        }
+        else{ // dish not exist
+            err = new Error('Dish' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err); // we're just going to return that error here from the get operation
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+// For route of /:dishId/comments/:commentId
+dishRouter.route('/:dishId/comments/:commentId')
+.get( (req,res,next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        if(dish != null && dish.comments.id(req.params.commentId) != null){
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(dish.comments.id(req.params.commentId));  // return the comments
+        }
+        else if (dish == null){ // dish not exist
+            err = new Error('Dish' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err); // we're just going to return that error here from the get operation
+        }
+        else{ // commentID doesn't exist
+            err = new Error('Comment ' + req.params.commentId + ' not found');
+            err.status = 404;
+            return next(err);
+            }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/'+ req.params.dishId 
+        + '/comments/' + req.params.commentId);
+})
+.put((req, res, next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        if(dish != null && dish.comments.id(req.params.commentId) != null){
+            // can update comments
+            /**if a comment already exists then I don't want to allow the user to change 
+             * the author of the comment, the author should retained. The only two fields 
+             * that I would allow the user update is the rating and the comment*/
+            if(req.body.rating){
+                dish.comments.id(req.params.commentId).rating = req.body.rating;
+            }
+            if(req.body.comment){
+                dish.comments.id(req.params.commentId).comment = req.body.comment;
+            }
+            dish.save() // save the collection 
+            .then((dish) => {
+                res.statusCode = 200;  // res for sending back the reply
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);  // returning the updated dish back to the user here.
+            }, (err) => next(err));
+        }
+        else if (dish == null){ // dish doesn't exist, cannot update comments
+            err = new Error('Dish' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err); // we're just going to return that error here from the get operation
+        }
+        else{ // commentID doesn't exist, cannot update comments
+            err = new Error('Comment' + req.params.commentId + ' not found');
+            err.status = 404;
+            return next(err);
+            }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.delete( (req, res, next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish) => { // resp, Server response
+        if(dish != null && dish.comments.id(req.params.commentId) != null){
+            /** have to go in and delete a specific comments */
+            dish.comments.id(req.params.commentId).remove();           
+            dish.save() // save the collection 
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);  // returning the updated dish back to the user here.
+            }, (err) => next(err));
+        }
+        else if (dish == null){ // dish doesn't exist, cannot update comments
+            err = new Error('Dish' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err); // we're just going to return that error here from the get operation
+         }
+        else{ // commentID doesn't exist, cannot update comments
+            err = new Error('Comment' + req.params.commentId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
     }, (err) => next(err))
     .catch((err) => next(err));
 });
