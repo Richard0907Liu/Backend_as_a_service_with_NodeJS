f2d5098e5e6367a8813e787eae62292635b98915
Richard Liu <richard0907@gmail.com>
2019-06-19 01:02:14 -0400

    File Upload

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
+
+
 
 // connect to mongodb server and the collection Dishes
 const mongoose = require('mongoose');
@@ -85,6 +88,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
+
 
 
 // Those two are additional error handlers
