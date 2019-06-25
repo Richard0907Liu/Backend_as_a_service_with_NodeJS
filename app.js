e69ee4aeefdb0565dc1f914d643108882ddc1264
Richard0907Liu <51534318+Richard0907Liu@users.noreply.github.com>
2019-06-24 21:32:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ var createError = require('http-errors');
 var express = require('express');  // to connect the Express Server
 var path = require('path');
 var cookieParser = require('cookie-parser'); // can use cookie
-/**Morgan doing this work for you, it is printing out, 
- * tracing this information */
+
 var logger = require('morgan'); 
 var session = require('express-session');
-// This takes the session as its parameters. This session referring to 'cookie-parser' that we've just imported on here
+
 var FileStore = require('session-file-store')(session); 
 var passport = require('passport');
 var authenticate = require('./authenticate');
@@ -35,16 +34,12 @@ connect.then((db) => {
 
 var app = express();
 
-// Secure traffic only
-/*This is port 3000 (http), it'll redirect that request to the secure port , let me 
-set up a middleware right after we declare the app.express. */
+
 app.all('*', (req, res, next) => {
   if(req.secure){ // the request  is already a secure req, the secure flag is true
     return next(); // just go to next step
   }
-  else{ // regular http
-    //Redirecting to the same server.  http://localhost:3000/dishes => 3443
-    // status code ad 307, here represents that the target resource resides temporarily under different URL
+  else{
     res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
   }
 }); // all() means all request no matter what the path in the request is.
@@ -57,23 +52,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-/** The String just a key that can be used by our cookie-parser in order to encrypt the information 
- * and sign the cookie that is sent from the server to the client */
-//app.use(cookieParser('12345-67890-09876-54321')); // signed cookie
 
-/** use token-base authenticate, not use session authenticate
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-*/
-
-// About passport
-/** So, passport.authenitcate('local') add "req.user" and then, the passport session that we have done here will 
- * automatically serialize that user information and then store it in the session. */
 app.use(passport.initialize());
 // app.use(passport.session()); not use session
 ////
